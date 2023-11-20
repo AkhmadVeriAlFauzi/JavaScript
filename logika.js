388e5eb51e9c39fd7b30ce3eea80de0d41edb0df
verry <alfauziveri@gmail.com>
2023-11-20 23:38:10 +0700

    first

---
+++ logika.js
@@ -0,0 +1,49 @@
+// WPU(2.3) = https://youtu.be/_XSeF00qNWE?list=PLFIM0718LjIWXagluzROrA-iBY9eeUt4w
+
+// operator aritmatika
+//  && and 
+// || or
+// ! not
+
+
+// x = 10
+// 10
+
+// (x % 2 == 0)
+// true 
+
+// (x % 2 == 0) && (x < 15)  [kanan dan kirinya harus true untuk menghasilkan true]
+// true 
+
+// (x % 2 == 0) || (x < 5) [jika salah satunya true maka pasti true]
+// true
+
+
+// Operator String
+console.log("[ operator string ]");
+
+var a = "apple"
+var b = "pan"
+
+// hasil(a + b) 
+console.log(a + b);
+
+var angka1 = 10
+var angka2 = 20
+
+console.log(angka1 + angka2);
+
+// operator type of
+var w = typeof(10)
+console.log(w);
+
+var y = typeof("halo")
+console.log(y);
+
+var z = typeof(true)
+console.log(z);
+
+// Operator Kondisional
+console.log("[ operator kondisional ]");
+
+// (i % 2 == 0) ? "genap" : "ganjil"
---
+++ operator perbandingan.js
@@ -0,0 +1,9 @@
+// WPU(2.2) = https://youtu.be/EnXClrVdpTM?si=8Ma-FrApz_lE-VrA
+
+// == samadengan
+//  != tidak samadengan
+// === strict samadengan = lebih spesifik, typedata nya harus sama 
+// !== strict tidak samadengan
+
+// akan menghasilkan nilai Boolean, yaitu true or false
+
